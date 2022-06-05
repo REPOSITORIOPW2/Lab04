e098eac38cfa943e255b973102092d282937ad11
kevinPedroYare <kyare@unsa.edu.pe>
2022-06-04 23:41:46 -0500

    Se hizo una prueba de creacion de la tabla

--- Tarea-del-Ajedrez/Ejercicio2g.py
+++ Tarea-del-Ajedrez/Ejercicio2g.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+#test 1
+#draw(under(rock+"etcpieces",chessboard))
+#draw(under(peon+"peones", chesboard))
+#draw(table)
+#draw(under(_invColor(rook+"torres", chessboard)))
+#draw(under(-invColor(peon+"peones", chesboard)))
+

40cbce32028297e4e19a3a9c41f15bffe80595c6
kevinPedroYare <kyare@unsa.edu.pe>
2022-06-05 22:35:38 -0500

    se añadio un test para la prueba de hacer la tabla

--- Tarea-del-Ajedrez/Ejercicio2g.py
+++ Tarea-del-Ajedrez/Ejercicio2g.py
@@ -2,9 +2,8 @@ from interpreter import draw
 from chessPictures import *
 
 #test 1
-#draw(under(rock+"etcpieces",chessboard))
-#draw(under(peon+"peones", chesboard))
-#draw(table)
-#draw(under(_invColor(rook+"torres", chessboard)))
-#draw(under(-invColor(peon+"peones", chesboard)))
-
+#draw(square.join(square.negative()).horizontalRepeat(4)
+# .under(rock.join(pawn).join(bishop).join(knight).join(king).join(queen).join(knight).join(bishop).join(pawn).join(rock)).negative()
+# .up(square.join(square.negative()).horizontalRepeat(4).verticalRepeat(4)
+# .(square.join(square.negative()).horizontalRepeat(4)
+# .under(rock.join(pawn).join(bishop).join(knight).join(king).join(queen).join(knight).join(bishop).join(pawn).join(rock)))
